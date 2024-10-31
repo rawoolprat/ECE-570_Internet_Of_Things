a87042b99296994c420aba58d0c2ac835507a98b
Pratiksha Rawool <rawoolp@sonoma.edu>
2024-10-31 00:57:56 -0700

    Create send.h

---
+++ notification_from_iffft_to_slack/send.h
@@ -0,0 +1,27 @@
+#ifndef SEND_H
+#define SEND_H
+
+#include <Arduino.h>
+
+class Switch {
+public:
+    // Constructor: takes the pin number as an argument
+    Switch(uint8_t pin);
+    
+    // Initializes the switch pin as input
+    void begin();
+    
+    // Checks if the switch is pressed
+    bool isPressed();
+
+private:
+    uint8_t _pin;  // Stores the pin number where the switch is connected
+};
+
+// Declare mySwitch as an external variable
+extern Switch mySwitch;
+
+// Function to send a notification to Slack via IFTTT
+void sendNotification();
+
+#endif
